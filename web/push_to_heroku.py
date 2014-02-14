6013f0521660a249538a691473c297738798d2ba
Jamieson Warner <jamiesonwarner@gmail.com>
2014-02-14 16:47:33 -0600

    Testing new dummy webserver

---
+++ web/push_to_heroku.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+
+import os
+script_dir = os.path.dirname(os.path.realpath(__file__))
+os.chdir(os.path.join(script_dir, ".."))
+os.system(r"git subtree push --prefix web heroku master")
