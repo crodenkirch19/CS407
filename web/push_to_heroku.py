c712ca41974b2061df3142af1cfd872e9878e6e3
Jamieson Warner <jamiesonwarner@gmail.com>
2014-02-26 16:45:38 -0600

    next push to heroku

--- web/push_to_heroku.py
+++ web/push_to_heroku.py
@@ -3,4 +3,5 @@
 import os
 script_dir = os.path.dirname(os.path.realpath(__file__))
 os.chdir(os.path.join(script_dir, ".."))
+# os.system(r"git commit -a -m ")
 os.system(r"git subtree push --prefix web heroku master")
