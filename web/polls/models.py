1589ab781e77ba06a335a8381ca80724bc747337
Jamieson Warner <jamiesonwarner@gmail.com>
2014-02-26 16:35:50 -0600

    fixed models.py format

--- web/polls/models.py
+++ web/polls/models.py
@@ -1,12 +1,10 @@
 from django.db import models
 
-
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
+class Poll(models.Model):
+    question = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
-
 class Choice(models.Model):
-    question = models.ForeignKey(Question)
+    poll = models.ForeignKey(Poll)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
