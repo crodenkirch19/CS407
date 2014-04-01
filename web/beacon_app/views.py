3673a683fe9f8e85f6bdf4be30ca2bf1ad197aa4
Jamieson Warner <jamiesonwarner@gmail.com>
2014-03-31 19:40:10 -0500

    added Bootstrap template to webpage

--- web/beacon_app/views.py
+++ web/beacon_app/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-
+from django.template import RequestContext, loader, Context, Template
 from django.http import HttpResponse
 import datetime
 
 def index(request):
     now = datetime.datetime.now()
     html = "<html><body>It is now %s.</body></html>" % now
-    return HttpResponse(html)
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render(Context()))
