c799c4c050264f200b8f24d7ff728cb6fb5b1bf7
Jamieson Warner <jamiesonwarner@gmail.com>
2014-04-03 15:36:07 -0500

    made a utility script to track environment variables

---
+++ web/get_env_variables.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# Script to export Heroku environment variables into the local shell
+# Should work with any Heroku setup
+# Run "source get_env_variables.sh" to get these into your shell
+
+cd ..
+
+name=""
+value=""
+for x in `heroku config | tail -n +2`; do
+  if [ "$name" = "" ] 
+  then
+    name="${x%?}" # remove trailing : 
+  else
+    value=$x
+    export $name=$value
+    name=""
+    value=""
+  fi
+done
