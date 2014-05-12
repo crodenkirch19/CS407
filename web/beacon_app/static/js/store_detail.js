b97813f05782bca9abdaabeac7019db89e729414
Jamieson Warner <jamiesonwarner@gmail.com>
2014-05-11 22:02:56 -0500

    added heatmap visualization

--- web/beacon_app/static/js/store_detail.js
+++ web/beacon_app/static/js/store_detail.js
@@ -4,46 +4,132 @@
 function Visualiser(root_container_id, beacons, store) {
     // Data array containing a list of beacons
     this.beacons = beacons;
-
     // Data object containing store metadata
     this.store = store;
+    this.root_id = root_container_id;
 
-    // d3 svg object we use for our interactive Visualiser    
-    this.svg = this.init(root_container_id);
+    this['heatmap'] = this.drawHeatmap;
+    this['polyline'] = this.drawPolyline;
 
-    this.draw_beacons(beacons);
+    this.vis_method = null;
 
+    // Load position scans from db
     var self = this;
     this.load_scans(function(scans) {
-        // 'heatmap', 'polyline'
-        self.draw_scans(scans, 'polyline');
+        self.scans = scans;
+        if (self.vis_method) {
+            self.visualize(self.vis_method);
+        }
     });
-}
+};
 
-Visualiser.prototype.draw_beacons = function(beacons) {
-    if (!beacons)
-        return;
+Visualiser.prototype.visualize = function(type) {
+    // If scans aren't loaded save vis type for later
+    this.vis_method = type;
+    if (!this.scans) {
+        return
+    }
 
-    for (var i = 0; i < beacons.length; i++) {
-        // Add this beacon as a circle on the svg overlay
-        // x, y, address
-        var beacon = beacons[i]; 
-        var beaconX = this.store.scale * beacon.x;
-        var beaconY = this.store.scale * beacon.y;
-        console.log(beaconX, beaconY);
-        this.svg.append("circle")
-            .attr("cx", beaconX)
-            .attr("cy", beaconY)
-            .attr("r", 12)
-            .style("fill", "FireBrick")
-            .style("stroke", "Maroon")
-            .style("stroke-width", "3");
+    if (this.hasOwnProperty(type)) {
+        this.cleanupStage();
+        this[type]();
+    }
+    else {
+        console.log("Error: unrecognized visualization method.");
     }
 };
 
-// TODO add more visualisations
-// TODO heatmap http://www.patrick-wied.at/static/heatmapjs/#
-// TODO cloud of points
+Visualiser.prototype.cleanupStage = function() {
+    // Remove any existing canvas or svg object in rootid
+    var root = document.getElementById(this.root_id);
+    while (root.firstChild) {
+        root.removeChild(root.firstChild);
+    }
+};
+
+Visualiser.prototype.drawHeatmap = function() {
+    console.log("Drawing heatmap")
+
+    // Set up store floorplan on a draggable 'map display'
+    // Use d3 zoom behavior for this
+    var width = 960,
+        height = 500;    
+    var imWidth = 5100,
+        imHeight = 3300;
+    scale = width / imWidth;
+
+    var bgImage = $('<img>', { 
+        src:this.store.image_url,
+        width:Math.round(scale*imWidth),
+        height:Math.round(scale*imHeight) 
+    });
+    $("#"+this.root_id).append(bgImage);
+
+
+    // Init heatmap canvas
+    var root_id = this.root_id;
+    var config = {
+        element: document.getElementById(root_id),
+        radius: 40,
+        opacity: 50
+    };
+    var heatmap = h337.create(config);
+
+    // Draw heatmap over the map
+    for (var i = 0; i < this.scans.length; i++) {
+        // Fill a string with all of the points to put in the polyline
+        var path = this.scans[i];
+        for (var j = 0; j < path.length; j++) {
+            var scan = path[j];
+            var storeX = scan.x * this.store.scale * scale;
+            var storeY = scan.y * this.store.scale * scale;
+            heatmap.store.addDataPoint(storeX, storeY, Math.floor(Math.random() * 3 + 2));        
+        }
+    }
+};
+
+Visualiser.prototype.drawPolyline = function() {
+    console.log("Drawing polyline");
+
+    // Init svg stage
+    var svg = this.init_svg(this.root_id);
+
+    // Draw beacons
+    for (var i = 0; i < this.beacons.length; i++) {
+        // Add this beacon as a circle on the svg overlay
+        // x, y, address
+        var beacon = this.beacons[i]; 
+        var beaconX = this.store.scale * beacon.x;
+        var beaconY = this.store.scale * beacon.y;
+        svg.append("circle")
+            .attr("cx", beaconX)
+            .attr("cy", beaconY)
+            .attr("r", 24)
+            .style("fill", "#ff4136")
+            .style("stroke", "#ff291c")
+            .style("stroke-width", "3");
+    }
+
+    // Draw a polyline for each scan
+    for (var i = 0; i < this.scans.length; i++) {
+        // Fill a string with all of the points to put in the polyline
+        var polyPoints = "";
+        var path = this.scans[i];
+        for (var j = 0; j < path.length; j++) {
+            var scan = path[j];
+            var storeX = scan.x * this.store.scale;
+            var storeY = scan.y * this.store.scale;
+            polyPoints += "" + storeX + "," + storeY + " ";
+        }
+
+        // Draw the polyline
+        svg.append("polyline")
+            .attr("points", polyPoints)
+            .style("fill","none")
+            .style("stroke-width","8")
+            .style("stroke","#0c516c");
+    }
+};
 
 Visualiser.prototype.draw_scans = function(scans, render_type) {
     // Scans is a nested array of user paths
@@ -63,25 +149,6 @@ Visualiser.prototype.draw_scans = function(scans, render_type) {
         };  
     }
     if (render_type == 'polyline') {
-        // Draw a polyline for each scan
-        for (var i = 0; i < scans.length; i++) {
-            // Fill a string with all of the points to put in the polyline
-            var polyPoints = "";
-            var path = scans[i];
-            for (var j = 0; j < path.length; j++) {
-                var scan = path[j];
-                var storeX = scan.x * this.store.scale;
-                var storeY = scan.y * this.store.scale;
-                polyPoints += "" + storeX + "," + storeY + " ";
-            }
-
-            // Draw the polyline
-            this.svg.append("polyline")
-                .attr("points", polyPoints)
-                .style("fill","none")
-                .style("stroke-width","4")
-                .style("stroke","Sienna");
-        }
     }
 };
 
@@ -103,7 +170,7 @@ Visualiser.prototype.load_scans = function(callback) {
 
 
 
-Visualiser.prototype.init = function(root_container_id) {
+Visualiser.prototype.init_svg = function(root_container_id) {
     var zoomed = function() {
       var translate = zoom.translate();
       content.attr("transform", 
